924bf6b3bbda8a5499ede873c03fbb9ee14ac176
Riegooo <christiandanielcagas0@gmail.com>
2026-09-03 17:09:10 +0800

    feat: add logging interceptor

--- src/common/interceptors/logging/logging.interceptor.ts
+++ src/common/interceptors/logging/logging.interceptor.ts
@@ -1,14 +1,22 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 
     const request = context.switchToHttp().getRequest();
+    const start = Date.now();
+
 
     console.log(`Incoming Request ${request.method} ${request.url}`);
 
-    return next.handle();
+    return next.handle().pipe(
+      tap(() => {
+        const duration = Date.now() - start;
+        console.log(`Request Completed in ${duration}`);
+      })
+    );
   }
 }
